b38a2eae18efec5862100b1e6dc22a47403e2c64
aldo armenta <AldoGuadalupe.Armenta@zkw.mx>
2023-12-07 00:01:52 -0600

    agregando ultima documentacion al proyecto

--- htmlExtData/models.py
+++ htmlExtData/models.py
@@ -2,19 +2,73 @@
 from django.db import models
 
 
+from django.db import models
+
 class HourlyComplianceReport(models.Model):
+    """
+    Modelo que representa los informes de cumplimiento por hora.
+
+    Attributes:
+        hour (IntegerField): La hora del informe (clave primaria).
+        quantity_per_hour (IntegerField): Cantidad por hora en el informe.
+        total_quantity (IntegerField): Cantidad total acumulada en el informe.
+    """
+
     hour = models.IntegerField(primary_key=True)
     quantity_per_hour = models.IntegerField()
     total_quantity = models.IntegerField()
 
     def __str__(self):
+        """
+        Representación de cadena del objeto HourlyComplianceReport.
+
+        Returns:
+            str: Representación de cadena del objeto.
+        """
         return f"{self.hour} - {self.quantity_per_hour} - {self.total_quantity}"
 
     class Meta:
+        """
+        Clase Meta para configuraciones adicionales del modelo.
+
+        Attributes:
+            db_table (str): Nombre de la tabla en la base de datos.
+        """
         db_table = 'HourlyComplianceReport'
 
 
+
+from django.db import models
+
 class complianceReport(models.Model):
+    """
+    Modelo que representa un informe de cumplimiento.
+
+    Attributes:
+        id_entry (AutoField): Identificador único del informe.
+        entryDate (CharField): Fecha de entrada del informe.
+        entryTime (CharField): Hora de entrada del informe.
+        MvT (CharField): Campo MvT del informe.
+        valType (CharField): Tipo de valor del informe (opcional).
+        MvtTypeTxt (CharField): Campo MvtTypeTxt del informe.
+        userName (CharField): Nombre de usuario del informe.
+        material (CharField): Material del informe.
+        quantity (CharField): Cantidad del informe.
+        EUn (CharField): Unidad de medida del informe.
+        LCAmount (CharField, opcional): Monto en LC del informe (opcional).
+        Crcy (CharField, opcional): Moneda del informe (opcional).
+        materialDescription (CharField): Descripción del material del informe.
+        matDoc (CharField): Documento de material del informe.
+        plnt (CharField): Campo plnt del informe.
+        numOrder (CharField): Número de orden del informe.
+        SLoc (CharField): Campo SLoc del informe.
+        batch (CharField): Lote del informe.
+        PO (CharField, opcional): Orden de compra del informe (opcional).
+        reas (CharField): Campo reas del informe.
+        pstngDate (CharField): Fecha de contabilización del informe.
+        costCtr (CharField): Centro de costos del informe.
+    """
+
     id_entry = models.AutoField(primary_key=True)
     entryDate = models.CharField(max_length=50)
     entryTime = models.CharField(max_length=50)
@@ -39,7 +93,20 @@ class complianceReport(models.Model):
     costCtr = models.CharField(max_length=50)
 
     def __str__(self):
+        """
+        Representación de cadena del objeto ComplianceReport.
+
+        Returns:
+            str: Representación de cadena del objeto.
+        """
         return f"{self.id_entry} - {self.material}"
 
     class Meta:
+        """
+        Clase Meta para configuraciones adicionales del modelo.
+
+        Attributes:
+            db_table (str): Nombre de la tabla en la base de datos.
+        """
         db_table = 'complianceReport'
+
--- htmlExtData/views.py
+++ htmlExtData/views.py
@@ -7,8 +7,10 @@ from datetime import datetime
 import pytz
 import win32com.client
 
+# Ruta principal para los archivos HTML (ajustar según sea necesario)
 mainRuteForHTMLDATA = 'C:\\Users\\MXARAD\\Downloads\\html\\'  # CAMBIAR ESTA RUTA CUANDO EL ARCHIVO SE MUEVA AL SERVIDOR
 
+# Respuestas HTTP predefinidas para el manejo de solicitudes
 responsesHTTP = {'success': {"response": 200},
                  'failed': {"response": 500},
                  'unknown': {"response": 200}
@@ -16,12 +18,23 @@ responsesHTTP = {'success': {"response": 200},
 
 
 def scraping_view(request):
-    file_path = mainRuteForHTMLDATA + 'Job REPORTEOKMCH, Step 1.htm'
+    """
+    Lee un archivo HTML, realiza scraping de las tablas específicas y guarda los datos en la base de datos.
 
+    Args:
+        request (HttpRequest): La solicitud HTTP.
+
+    Returns:
+        JsonResponse: Un objeto JSON que contiene los datos raspados.
+    """
+    # Ruta del archivo HTML
+    file_path = mainRuteForHTMLDATA + 'Job REPORTEOKMCH, Step 1.htm' # el nombre del archivo puede varias, considera en cambiarlo si esto sucede
+
+    # Lee el contenido HTML del archivo
     with open(file_path, 'r', encoding='utf-8') as file:
         html_content = file.read()
 
-    # Crear un objeto BeautifulSoup
+    # Crea un objeto BeautifulSoup para analizar el HTML
     soup = BeautifulSoup(html_content, 'html.parser')
 
     # Ejemplo de raspado: obtener las últimas 6 tablas
@@ -41,18 +54,62 @@ def scraping_view(request):
             values = [value.text.strip() for value in row.find_all('td')]
             data_dict = dict(zip(keys, values))
             all_scraped_data.append(data_dict)
-
+    # elimina la primera tabla del reporte ya que solo es una tabla informativa y no tiene datos relevantes
     del all_scraped_data[len(all_scraped_data) - 1]
 
-    # Crear el diccionario final con la clave 'count' y 'data'
+    # Crear el diccionario final con la clave 'data'
     data_dict = {'data': all_scraped_data}
 
+    # Insertar los datos en la base de datos
     insertData(data_dict['data'])
+
     # Devolver el resultado como JsonResponse
     return JsonResponse(data_dict, json_dumps_params={'indent': 2})
 
+def insertData(dataList_to_insert):
+    """
+    Limpia y convierte las fechas y horas en una lista de diccionarios y luego inserta los datos en la base de datos.
+
+    Args:
+        dataList_to_insert (list): Lista de diccionarios con datos a insertar en la base de datos.
+
+    Returns:
+        None
+    """
+    # Limpia los datos
+    clean_data = clean_data_list(dataList_to_insert)
+
+    # Convierte las fechas y horas a la zona horaria de México
+    changing_date = date_converter(clean_data)
+
+    # Verifica si la tabla está vacía
+    if complianceReport.objects.count() == 0:
+        # Si está vacía, realiza una inserción directa
+        for data_item in changing_date:
+            modelInsert(data_item)
+    else:
+        # Si no está vacía, realiza la validación de existencia de datos
+        for data_item in changing_date:
+            existing_data = complianceReport.objects.filter(entryDate=data_item['Entry Date'],
+                                                            entryTime=data_item['Time']).first()
+            if not existing_data:
+                modelInsert(data_item)
+
 
 def download_outlook_attachments(request):
+    """
+    Vista de Django para descargar archivos adjuntos de Outlook.
+
+    Utiliza la librería win32com para acceder a Outlook y descargar en la ruta definida de la variable global
+    'mainRuteForHTMLDATA' el último archivo adjunto
+    del buzón de correo especificado.
+
+    Args:
+        request: Solicitud HTTP.
+
+    Returns:
+        JsonResponse: Respuesta JSON indicando el resultado de la descarga.
+    """
     outlook_app = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
     store = outlook_app.stores('andonmch@zkw.mx')
     folder = store.GetRootFolder().Folders['scrapReports']
@@ -78,25 +135,17 @@ def download_outlook_attachments(request):
     else:
         return JsonResponse(responsesHTTP['failed'], safe=False)
 
-
-def insertData(dataList_to_insert):
-    clean_data = clean_data_list(dataList_to_insert)
-    changing_date = date_converter(clean_data)
-    # Verifica si la tabla está vacía
-    if complianceReport.objects.count() == 0:
-        # Si está vacía, realiza una inserción directa
-        for data_item in changing_date:
-            modelInsert(data_item)
-    else:
-        # Si no está vacía, realiza la validación de existencia de datos
-        for data_item in changing_date:
-            existing_data = complianceReport.objects.filter(entryDate=data_item['Entry Date'],
-                                                            entryTime=data_item['Time']).first()
-            if not existing_data:
-                modelInsert(data_item)
-
-
 def modelInsert(data_item):
+    """
+    Inserta un nuevo registro en la base de datos ZKW_testBoardResult en la tabla complianceReport.
+
+    Args:
+        data_item (dict): Un diccionario que contiene datos para el nuevo registro.
+
+    Returns:
+        None
+    """
+    # Crea una nueva instancia de complianceReport con los datos proporcionados
     data_instance = complianceReport(
         entryDate=data_item['Entry Date'],
         entryTime=data_item['Time'],
@@ -120,10 +169,21 @@ def modelInsert(data_item):
         pstngDate=data_item['Pstng Date'],
         costCtr=data_item['Cost Ctr'],
     )
+
+    # Guarda la instancia en la base de datos
     data_instance.save()
 
 
 def date_converter(data_list):
+    """
+    Convierte las fechas y horas en una lista de diccionarios desde la zona horaria de Austria (GMT+1) a la zona horaria de México (GMT-6).
+
+    Args:
+        data_list (list): Lista de diccionarios con fechas y horas en formato original.
+
+    Returns:
+        list: Lista de diccionarios con fechas y horas convertidas a la zona horaria de México.
+    """
     # Define las zonas horarias
     austria_tz = pytz.timezone("Europe/Vienna")  # GMT+1 (Austria)
     mexico_tz = pytz.timezone("America/Mexico_City")  # GMT-6 (Ciudad de México)
@@ -155,17 +215,27 @@ def date_converter(data_list):
     return data_list
 
 
+
 def clean_data_list(data_list):
+    """
+    Limpia una lista de diccionarios eliminando caracteres especiales y reemplazando espacios no rompibles.
+
+    Args:
+        data_list (list): Lista de diccionarios que se van a limpiar.
+
+    Returns:
+        list: Lista de diccionarios limpios.
+    """
     cleaned_data_list = []
 
     for data_dict in data_list:
         cleaned_data = {}
 
         for key, value in data_dict.items():
-            # Reemplazar espacios no rompibles con espacios regulares
+            # Reemplazar espacios no rompibles con espacios regulares en la clave
             cleaned_key = key.replace('\xa0', ' ')
 
-            # Reemplazar otros caracteres especiales si es necesario
+            # Reemplazar otros caracteres especiales en la clave si es necesario
             cleaned_key = cleaned_key.replace('\r', ' ').replace('\n', ' ')
 
             # Reemplazar espacios no rompibles en el valor
@@ -182,10 +252,18 @@ def clean_data_list(data_list):
     return cleaned_data_list
 
 
+
 def getcomplianceReport(request):
+    """
+    Retorna todos los registros de complianceReport como un objeto JSON.
+
+    Returns:
+        JsonResponse: Un objeto JSON que contiene todos los registros de complianceReport.
+    """
+    # Obtén todos los registros de complianceReport desde la base de datos
     data = complianceReport.objects.all()
 
-    # Crear una lista de diccionarios con los campos que deseas incluir en el JSON
+    # Crea una lista de diccionarios con los campos deseados para cada registro
     json_data = [
         {
             'id_entry': entry.id_entry,
@@ -214,11 +292,20 @@ def getcomplianceReport(request):
         for entry in data
     ]
 
+    # Retorna los datos como un objeto JSON
     return JsonResponse(json_data, safe=False)
 
 def getHourlyComplianceReport(request):
+    """
+    Retorna todos los registros de HourlyComplianceReport como un objeto JSON.
+
+    Returns:
+        JsonResponse: Un objeto JSON que contiene todos los registros de HourlyComplianceReport.
+    """
+    # Obtén todos los registros de HourlyComplianceReport desde la base de datos
     data = HourlyComplianceReport.objects.all()
 
+    # Crea una lista de diccionarios con los campos deseados para cada registro
     json_data = [
         {
             'hour': row.hour,
@@ -227,6 +314,9 @@ def getHourlyComplianceReport(request):
         }
         for row in data
     ]
+
+    # Retorna los datos como un objeto JSON
     return JsonResponse(json_data, safe=False)
+
 def welcomeMesage(request):
     return HttpResponse('hola mundo')
